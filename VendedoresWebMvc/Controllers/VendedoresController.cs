6bbcb2045007fc1b57c66ff09a93601c603bde5b
Pablo <pablo.henrique.prestes@gmail.com>
2024-01-22 19:21:36 -0300

    Controller vendedores

---
+++ VendedoresWebMvc/Controllers/VendedoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendedoresWebMvc.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
